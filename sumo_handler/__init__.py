a82e4b9c24615eabd06e4a43fc991ba9d11bf374
Erwin Kooi <erwin.kooi@empire.ca>
2019-11-21 14:57:22 -0500

    metrics

--- sumo_handler/__init__.py
+++ sumo_handler/__init__.py
@@ -13,6 +13,20 @@ from queue import Queue, Full, Empty
 instances = []  # For keeping track of running class instances
 
 
+class Metrics:
+    metrics: str
+    content_type: str
+    meta_data: dict
+
+    def __str__(self):
+        return self.metrics
+
+    def __init__(self, payload, content_type='application/vnd.sumologic.prometheus', meta_data=dict):
+        self.metrics = payload
+        self.content_type = content_type
+        self.meta_data = meta_data
+
+
 # Called when application exit imminent (main thread ended / got kill signal)
 @atexit.register
 def perform_exit():
@@ -33,6 +47,8 @@ def force_flush():
 
 class LogFormatter:
     def format_record(self, record: logging.LogRecord):
+        if isinstance(record.msg, Metrics):
+            return record.msg
         return {
             'filename': record.filename,
             'created': record.created,
@@ -159,6 +175,50 @@ class SumoHandler(logging.Handler):
         self.write_debug_log("format_record() called")
         return self.formatter.format_record(record)
 
+    def _post_payload(self, str_payload, json_payload, headers):
+        r = self.session.post(
+            self.url,
+            data=str_payload,
+            json=json_payload,
+            headers=headers,
+            verify=self.verify_https,
+            timeout=self.timeout
+        )
+        r.raise_for_status()  # Throws exception for 4xx/5xx status
+
+    def _send_payloads(self, payloads: list):
+        if len(payloads) == 1:
+            return self._send_payload(payloads[0])
+
+        logs = []
+        for item in payloads:
+            if isinstance(item, Metrics):
+                self._send_payload(item)
+            else:
+                logs.append(item)
+        if len(logs) > 0:
+            self._send_payload(
+                "\n".join(json.dumps(item) for item in logs)
+            )
+
+    def _send_payload(self, payload):
+        if not payload:
+            return
+
+        if isinstance(payload, str):
+            return self._post_payload(payload, None, None)
+
+        if isinstance(payload, Metrics):
+            self._post_payload(str(payload), None, {'Content-Type': payload.content_type})
+            if payload.meta_data:
+                # send the metrics also as json log line
+                new_payload = payload.meta_data
+                new_payload['metrics'] = payload.metrics
+                self._send_payload(new_payload)
+            return
+
+        self._post_payload(None, payload, {'Content-Type': 'application/json'})
+
     def _sumo_worker(self):
         if not self.enabled:
             return
@@ -170,23 +230,16 @@ class SumoHandler(logging.Handler):
             # via the timer, does not if invoked by force_flush()
             self.timer.cancel()
 
-        payload = self.fetch_payload()
+        payloads = self.fetch_payloads()
 
-        if payload:
+        if payloads:
             self.write_debug_log("Payload available for sending")
             self.write_debug_log("Destination URL is " + self.url)
 
             try:
-                self.write_debug_log("Sending payload: " + payload)
-
-                r = self.session.post(
-                    self.url,
-                    data=payload if isinstance(payload, str) else json.dumps(payload),
-                    verify=self.verify_https,
-                    timeout=self.timeout
-                )
-                r.raise_for_status()  # Throws exception for 4xx/5xx status
-                self.write_debug_log("Payload sent successfully")
+                self.write_debug_log("Sending payloads")
+                self._send_payloads(payloads)
+                self.write_debug_log("Payloads sent successfully")
 
             except Exception as e:
                 try:
@@ -215,7 +268,7 @@ class SumoHandler(logging.Handler):
                 self.timer.start()
                 self.write_debug_log("Timer thread scheduled")
 
-    def fetch_payload(self):
+    def fetch_payloads(self):
         result = []
         while not self.queue.empty():
             self.write_debug_log("Recursing through queue")
@@ -232,7 +285,7 @@ class SumoHandler(logging.Handler):
                 self.write_debug_log("Payload maximum size exceeded, sending immediately")
                 break
 
-        return "\n".join(json.dumps(item) for item in result)
+        return result
 
     def force_flush(self):
         if not self.enabled:
