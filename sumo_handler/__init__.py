7e6859468620dd35500e05ca692f93538c0c3918
Nick Thacker <nick.thacker@empire.ca>
2023-11-08 13:39:30 -0500

    Updating method_whitelist to allowed_methods

--- sumo_handler/__init__.py
+++ sumo_handler/__init__.py
@@ -116,7 +116,7 @@ class SumoHandler(logging.Handler):
         self.write_debug_log("Preparing to create a Requests session")
         retry = Retry(total=self.retry_count,
                       backoff_factor=self.retry_backoff,
-                      method_whitelist=False,  # Retry for any HTTP verb
+                      allowed_methods=False,  # Retry for any HTTP verb
                       status_forcelist=[500, 502, 503, 504])
         self.session.mount('https://', HTTPAdapter(max_retries=retry))
 
